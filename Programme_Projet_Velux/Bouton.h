f14653606bc12feb3ac5f5e3f0de5e9083a05b5e
louisonmalinge <louison.malinge@gmail.com>
2026-05-25 11:33:48 +0200

    Delete Programme_Projet_Velux directory

--- Programme_Projet_Velux/Bouton.h
+++
@@ -1,8 +0,0 @@
-#ifndef BOUTON_H
-#define BOUTON_H
-
-void initialiserBouton(); // Déclarer la fonction pour initialiser le bouton
-
-bool traiterEtatBouton();  // Déclarer la fonction pour lire l'état du bouton
-
-#endif
